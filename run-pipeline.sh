a393f82118ce0e62518ce01b013b323c0b32930e
Smart Pipeline <admin@smart-pipeline.local>
2026-09-14 22:41:14 -0400

    Suppress oc whoami errors in namespace default

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-NAMESPACE=${1:-$(oc whoami)-build}
+NAMESPACE=${1:-$(oc whoami 2>/dev/null)-build}
 REPO=$(git remote get-url origin)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
